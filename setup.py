3fe3a4c52cb44ee93fbbc57a4eef52f08df170c3
Raison Dsouza <raisondsouza@gmail.com>
2013-04-12 19:16:47 +0530

    temp change all to linkedin1

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
 
-setup(name = "python-linkedin",
-      version = "1.8.3",
+setup(name = "python-linkedin1",
+      version = "1.8.5",
       description = "Python Interface for the LinkedIn API",
       author = "Ozgur Vatansever",
       license = open("LICENSE").read(),
       long_description = open("README").read(),
       author_email = "ozgurvt@gmail.com",
       url = "http://code.google.com/p/python-linkedin/",
-      packages = ["linkedin"],
+      packages = ["linkedin1"],
       platforms = ["linux"])
